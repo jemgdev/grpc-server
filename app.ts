24c9fab8668dcebf256537ad65e4023e760b0ea6
josue <josue.medina@pragma.com.co>
2023-07-26 11:49:46 -0500

    Improve logic

--- app.ts
+++ app.ts
@@ -4,6 +4,7 @@ import * as protoLoader from '@grpc/proto-loader'
 import { ProtoGrpcType } from './proto/creation'
 import { CreationHandlers } from './proto/creationPackage/Creation'
 import { CreationRequest } from './proto/creationPackage/CreationRequest'
+import { CreationResponse } from './proto/creationPackage/CreationResponse'
 
 const PORT = 8080
 
@@ -13,29 +14,35 @@ const packageOf = protoLoader.loadSync(path.resolve(__dirname, PROTO_FILE))
 const grpcObject = (grpc.loadPackageDefinition(packageOf) as unknown) as ProtoGrpcType
 const creationPackage = grpcObject.creationPackage
 
+const callObjects = new Map<string, grpc.ServerDuplexStream<CreationRequest, CreationResponse>>()
+
 function getConnection () {
   const server = new grpc.Server()
   server.addService(creationPackage.Creation.service, {
     CreateItem: (call) => {
       call.on('data', (chunk: CreationRequest) => {
         const moduleName = call.metadata.get('moduleName')[0] as string
+        console.log(`${moduleName} => ${chunk.message}`)
 
+        if (callObjects.get('moduleName') === undefined) {
+          callObjects.set(moduleName, call)
+        }
+        
         if (moduleName === 'userModule') {
           const userId = chunk.userId!
 
-          console.log(chunk)
-
           if (!userId) {
-            call.write({ message: 'You must to specify a userId', status: false })
-            call.end()
+            for(let [module, moduleCall] of callObjects) {
+              if (module === 'userModule') {
+                moduleCall.write({ message: 'Must specify a user id', status: false })
+              }
+            }
           }
 
-          call.write({ message: 'userId received', userId, status: true })
-          call.end()
-        } else {
-          call.write({ message: 'Module name is not valid', status: false })
-          call.end()
-        }
+          for(let [module, moduleCall] of callObjects) {
+            moduleCall.write({ message: 'userId received', userId, status: true })
+          }
+        }        
       })
     }
   } as CreationHandlers)
